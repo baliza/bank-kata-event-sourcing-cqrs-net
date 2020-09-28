57b94788c47778eabb8844e905d9f1dba608c1dd
Eduardo Salvador <eduardo.salvador@col.vueling.com>
2020-09-28 12:04:49 +0200

    first commit

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace src
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			Console.WriteLine("An honest man is always a child!");
+		}
+	}
+}
